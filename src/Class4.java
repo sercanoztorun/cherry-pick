15c89e2519a68c1a83651ce0519d3a9af06fe3b2
Sercan Oztorun <enver.oztorun@consultant.turkcell.com.tr>
2021-01-11 16:54:10 +0300

    [Sercan] sinilecek log silindi yeni log eklendi.

--- src/Class4.java
+++ src/Class4.java
@@ -1,6 +1,7 @@
 public class Class4 {
     public static void main(String args[]) {
         System.out.println("cherry-pick test4");
-        System.out.println("cherry pick silinecek log");
+
+        System.out.println("log silindi yerine bu log geldi.");
     }
 }
