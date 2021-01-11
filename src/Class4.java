891f13605351ce1890c307f4cbde3e0d69c4cfd3
Sercan Oztorun <enver.oztorun@consultant.turkcell.com.tr>
2021-01-11 16:53:35 +0300

    [Sercan] sinilecek log eklendi.

--- src/Class4.java
+++ src/Class4.java
@@ -1,5 +1,6 @@
 public class Class4 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("cherry-pick test4");
+        System.out.println("cherry pick silinecek log");
     }
 }
