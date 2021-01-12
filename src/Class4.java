e87cd007a37b56de94a909d531df2bc7a14f903a
Sercan Oztorun <enver.oztorun@consultant.turkcell.com.tr>
2021-01-13 00:03:39 +0300

    commit E

--- src/Class4.java
+++ src/Class4.java
@@ -2,4 +2,7 @@ public class Class4 {
     public static void main(String args[]){
         System.out.println("cherry-pick test4");
     }
+    public void e(){
+        System.out.println("E");
+    }
 }
